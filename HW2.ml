663f7d01d9ef2195e3ce6a5b5cfdf6890fe07332
nghi-huynh <43221957+nghi-huynh@users.noreply.github.com>
2019-09-16 13:40:01 -0400

    Update HW2.ml

--- HW2.ml
+++ HW2.ml
@@ -1,3 +1,42 @@
+exception NotImplemented
+
+(* The type of binary numbers. *)
+type bnum =
+  | E
+  | Zero of bnum
+  | One of bnum
+
+(* The type of propositions. *)
+type prop =
+  | Atom of string
+  | Neg of prop
+  | Conj of prop * prop
+  | Disj of prop * prop
+  | Impl of prop * prop
+
+(* TODO: Write a good set of tests for intToBin. *)
+let intToBin_tests = [(6,Zero (One (One E)));(4, Zero (Zero (One E)));(1, One E);(0,E)];;
+(* TODO: Implement intToBin. *)
+let rec intToBin n =
+  raise NotImplemented
+(* TODO: Write a good set of tests for binToInt. *)
+let binToInt_tests = [(E,0);(Zero (One (One (Zero E))),6);(Zero (Zero (One E)),4);(One E,1)];;
+(* TODO: Implement binToInt. *)
+let rec binToInt b =
+  raise NotImplemented
+(* TODO: Write a good set of tests for nnf. *)
+let nnf_tests = [(Conj(Atom "p",Neg(Disj (Atom "q",Atom"r"))),Conj ( Atom "p",Conj(Neg(Atom "q"),Neg(Atom"r"))));
+                 (Neg (Neg (Atom"a")),Atom "a");
+                 (Impl(Neg (Atom "a"),Neg (Atom "b")),Disj (Atom "a",Neg (Atom "b")));
+                 (Neg(Disj(Neg(Atom "a"),Neg(Atom "b"))), Conj(Atom "a", Atom "b"));
+                 (Neg(Conj(Neg((Neg(Atom "a"))),Atom "b")), Disj(Neg(Atom "a"), Neg(Atom "b")));
+                ];;
+(* TODO: Implement nnf. *)
+let rec nnf p =
+  raise NotImplemented
+
+
+
 (* TODO: Write a good set of tests for psum. *)
 let psum_tests = [([1;2;1],[1;3;4]);([0;1;4],[0;1;5]);([1],[1]);([],[])];;
 
